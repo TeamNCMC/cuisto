c9f16458a59aa081cd34b35ad555b82763c58f36
glegoc@NeuroPSI <g.legoc@posteo.org>
2025-01-23 12:30:41 +0100

    Added insertDetectionsInHierarchy.groovy

---
+++ scripts/qupath-utils/tools/insertDetectionsInHierarchy.groovy
@@ -0,0 +1,8 @@
+/**
+ * insertDetectionsInHierarchy.groovy
+ *
+ * Insert detections into the hierarchy so that they have the corrent "Parent".
+ */
+
+// Insert in hierarchy
+insertObjects(getDetectionObjects())
