e95e2c77787b29710bd1d802a7e001481f6d446c
Guillaume Le Goc <g.legoc@posteo.org>
2025-05-23 10:30:47 +0200

    Update cells_distributions.py, density_map.py, fibers_coverage.ipynb, and 1 more file

---
+++ examples/cells_distributions.py
@@ -0,0 +1,89 @@
+# This script shows how to load data exported from QuPath, compute metrics and display
+# them, according to the configuration file. This is meant for a single-animal.
+#
+# There are some conventions that need to be met in the QuPath project so that the
+# measurements are usable with `cuisto`:
+# + Objects' classifications must be derived, eg. be in the form "something: else". The
+#   primary classification ("something") will be refered to "object_type" and the
+#   secondary classification ("else") to "channel" in the configuration file.
+# + Only one "object_type" can be processed at once, but supports any numbers of
+#   channels.
+# + Annotations (brain regions) must have properly formatted measurements. For punctual
+#   objects, it would be the count. Run the "add_regions_count.groovy" script to add
+#   them. The measurements names must be in the form "something: else name", for
+#   instance, "something: else Count". "name" is refered to "base_measurement" in the
+#   configuration file.
+#
+# The data was generated from QuPath with stardist cell detection followed by a pixel
+# classifier "Classify" function on toy data.
+#
+# If you cloned the repository, this example should be ready to run.
+#
+# Find this script as a notebook in the documentation :
+# https://teamncmc.github.io/cuisto/demo_notebooks/cells_distributions.html
+
+from pathlib import Path
+
+import pandas as pd
+
+import cuisto
+
+# Full path to your configuration file, edited according to your need beforehand
+wdir = Path(__file__).parent.parent
+config_file = wdir / "resources" / "demo_config_cells.toml"
+
+# - Files
+# animal identifier
+animal = "animalid0"
+# set the full path to the annotations tsv file from QuPath
+annotations_file = "../../resources/cells_measurements_annotations.tsv"
+# set the full path to the detections tsv file from QuPath
+detections_file = "../../resources/cells_measurements_detections.tsv"
+
+# get configuration
+cfg = cuisto.config.Config(config_file)
+
+# read data
+df_annotations = pd.read_csv(annotations_file, index_col="Object ID", sep="\t")
+df_detections = pd.read_csv(detections_file, index_col="Object ID", sep="\t")
+
+# remove annotations that are not brain regions
+df_annotations = df_annotations[df_annotations["Classification"] != "Region*"]
+df_annotations = df_annotations[df_annotations["ROI"] != "Rectangle"]
+
+# convert atlas coordinates from mm to microns
+df_detections[["Atlas_X", "Atlas_Y", "Atlas_Z"]] = df_detections[
+    ["Atlas_X", "Atlas_Y", "Atlas_Z"]
+].multiply(1000)
+
+# have a look
+print(df_annotations.head())
+print(df_detections.head())
+
+# get distributions per regions, spatial distributions and coordinates
+df_regions, dfs_distributions, df_coordinates = cuisto.process.process_animal(
+    animal, df_annotations, df_detections, cfg, compute_distributions=True
+)
+
+# have a look
+print(df_regions.head())
+print(df_coordinates.head())
+
+# plot distributions per regions
+figs_regions = cuisto.display.plot_regions(df_regions, cfg)
+# specify which regions to plot
+# figs_regions = cuisto.display.plot_regions(df_regions, cfg, names_list=["GRN", "IRN", "MDRNv"])
+
+# save as svg
+# figs_regions[0].savefig(r"C:\Users\glegoc\Downloads\regions_count.svg")
+# figs_regions[1].savefig(r"C:\Users\glegoc\Downloads\regions_density.svg")
+
+# plot 1D distributions
+fig_distrib = cuisto.display.plot_1D_distributions(
+    dfs_distributions, cfg, df_coordinates=df_coordinates
+)
+# If there were several `animal` in the measurement file, it would be displayed as
+# mean +/- sem instead.
+
+# plot heatmap (all types of cells pooled)
+fig_heatmap = cuisto.display.plot_2D_distributions(df_coordinates, cfg)
---
+++ examples/density_map.py
@@ -0,0 +1,162 @@
+# Draw 2D heatmaps as density isolines.
+#
+# This script does not actually use `cuisto` and relies only on
+# brainglobe-heatmap (https://brainglobe.info/documentation/brainglobe-heatmap/index.html)
+# to extract brain structures outlines.
+#
+# Only the detections measurements with atlas coordinates exported from QuPath are used.
+#
+# You need to select the range of data to be used, the regions outlines will be
+# extracted at the centroid of that range. Therefore, a range that is too large will be
+# misleading and irrelevant.
+#
+# If you cloned the repository, this example should be ready to run.
+#
+# Find this script as a notebook in the documentation :
+# https://teamncmc.github.io/cuisto/demo_notebooks/density_map.html
+
+import brainglobe_heatmap as bgh
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# path to the exported measurements from QuPath
+filename = "../resources/cells_measurements_detections.tsv"
+
+# Settings
+# atlas to use
+atlas_name = "allen_mouse_10um"
+# brain regions whose outlines will be plotted
+regions = ["root", "CB", "MY", "GRN", "IRN"]
+# range to include, in Allen coordinates, in microns
+ap_lims = [9800, 10000]  # lims : [0, 13200] for coronal
+ml_lims = [5600, 5800]  # lims : [0, 11400] for sagittal
+dv_lims = [3900, 4100]  # lims : [0, 8000] for top
+# number of isolines
+nlevels = 5
+# color mapping between classification and matplotlib color
+palette = {"Cells: marker-": "#d8782f", "Cells: marker+": "#8ccb73"}
+
+df = pd.read_csv(filename, sep="\t")
+print(df.head())
+
+# Here we can filter out classifications we don't wan't to display.
+# select objects
+# df = df[df["Classification"] == "example: classification"]
+
+# get outline coordinates in coronal (=frontal) orientation
+coords_coronal = bgh.get_structures_slice_coords(
+    regions,
+    orientation="frontal",
+    atlas_name=atlas_name,
+    position=(np.mean(ap_lims), 0, 0),
+)
+# get outline coordinates in sagittal orientation
+coords_sagittal = bgh.get_structures_slice_coords(
+    regions,
+    orientation="sagittal",
+    atlas_name=atlas_name,
+    position=(0, 0, np.mean(ml_lims)),
+)
+# get outline coordinates in top (=horizontal) orientation
+coords_top = bgh.get_structures_slice_coords(
+    regions,
+    orientation="horizontal",
+    atlas_name=atlas_name,
+    position=(0, np.mean(dv_lims), 0),
+)
+
+# Coronal projection
+# select objects within the rostro-caudal range
+df_coronal = df[
+    (df["Atlas_X"] >= ap_lims[0] / 1000) & (df["Atlas_X"] <= ap_lims[1] / 1000)
+]
+
+plt.figure()
+
+for struct_name, contours in coords_coronal.items():
+    for cont in contours:
+        plt.fill(cont[:, 0] / 1000, cont[:, 1] / 1000, lw=1, fc="none", ec="k")
+
+# see https://seaborn.pydata.org/generated/seaborn.kdeplot.html to customize
+ax = sns.kdeplot(
+    df_coronal,
+    x="Atlas_Z",
+    y="Atlas_Y",
+    hue="Classification",
+    levels=nlevels,
+    common_norm=False,
+    palette=palette,
+)
+ax.invert_yaxis()
+sns.despine(left=True, bottom=True)
+plt.axis("equal")
+plt.xlabel(None)
+plt.ylabel(None)
+plt.xticks([])
+plt.yticks([])
+plt.plot([2, 3], [8, 8], "k", linewidth=3)
+plt.text(2, 7.9, "1 mm")
+
+# Sagittal projection
+# select objects within the medio-lateral range
+df_sagittal = df[
+    (df["Atlas_Z"] >= ml_lims[0] / 1000) & (df["Atlas_Z"] <= ml_lims[1] / 1000)
+]
+
+plt.figure()
+
+for struct_name, contours in coords_sagittal.items():
+    for cont in contours:
+        plt.fill(cont[:, 0] / 1000, cont[:, 1] / 1000, lw=1, fc="none", ec="k")
+
+# see https://seaborn.pydata.org/generated/seaborn.kdeplot.html to customize
+ax = sns.kdeplot(
+    df_sagittal,
+    x="Atlas_X",
+    y="Atlas_Y",
+    hue="Classification",
+    levels=nlevels,
+    common_norm=False,
+    palette=palette,
+)
+ax.invert_yaxis()
+sns.despine(left=True, bottom=True)
+plt.axis("equal")
+plt.xlabel(None)
+plt.ylabel(None)
+plt.xticks([])
+plt.yticks([])
+plt.plot([2, 3], [7.1, 7.1], "k", linewidth=3)
+plt.text(2, 7, "1 mm")
+
+# Top projection
+# select objects within the dorso-ventral range
+df_top = df[(df["Atlas_Y"] >= dv_lims[0] / 1000) & (df["Atlas_Y"] <= dv_lims[1] / 1000)]
+
+plt.figure()
+
+for struct_name, contours in coords_top.items():
+    for cont in contours:
+        plt.fill(-cont[:, 0] / 1000, cont[:, 1] / 1000, lw=1, fc="none", ec="k")
+
+# see https://seaborn.pydata.org/generated/seaborn.kdeplot.html to customize
+ax = sns.kdeplot(
+    df_top,
+    x="Atlas_Z",
+    y="Atlas_X",
+    hue="Classification",
+    levels=nlevels,
+    common_norm=False,
+    palette=palette,
+)
+ax.invert_yaxis()
+sns.despine(left=True, bottom=True)
+plt.axis("equal")
+plt.xlabel(None)
+plt.ylabel(None)
+plt.xticks([])
+plt.yticks([])
+plt.plot([0.5, 1.5], [0.5, 0.5], "k", linewidth=3)
+plt.text(0.5, 0.4, "1 mm")
